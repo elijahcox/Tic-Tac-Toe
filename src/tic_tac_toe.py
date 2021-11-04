6153990c2e2af9aaac51ce7960677db6c0e14853
Elijah Cox <elijahtom@gmail.com>
2021-11-04 17:56:01 -0500

    add move order detection and sanitization

--- src/tic_tac_toe.py
+++ src/tic_tac_toe.py
@@ -10,4 +10,10 @@ def usage():
     print("To play, simply enter 1-9 corresponding to the cell that you want to play on.")
 
 move_arr = [0]*9
+move_order = ""
 usage()
+
+while move_order.upper() != 'X' and move_order.upper() != 'O':
+    move_order = str(input("Enter X to play first, O to play second. Enter Q to quit. Choice: "))
+    if move_order.upper() == 'Q':
+        exit()
