e92fb56c0cfba0fe332ea83244581264d6f43b68
Elijah Cox <elijahtom@gmail.com>
2021-11-04 17:50:02 -0500

    add usage utility, set up input handling

--- src/tic_tac_toe.py
+++ src/tic_tac_toe.py
@@ -1,1 +1,13 @@
 #pass
+
+def print_board(move_list):
+    pass
+
+def usage():
+    print("Welcome to impossible Tic Tac Toe!\nThe board is laid out as follows:")
+    moves = [1,2,3,4,5,6,7,8,9]
+    print_board(moves)
+    print("To play, simply enter 1-9 corresponding to the cell that you want to play on.")
+
+move_arr = [0]*9
+usage()
