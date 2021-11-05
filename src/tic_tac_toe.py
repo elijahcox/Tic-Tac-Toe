535d3892f26a984c876b62efce92a3eeb43a1a85
Elijah Cox <elijah.thomas.cox@gmail.com>
2021-11-05 09:39:03 -0500

    init cpu move structure

--- src/tic_tac_toe.py
+++ src/tic_tac_toe.py
@@ -1,4 +1,29 @@
-#pass
+
+'''
+    Win: If you have two in a row, play the third to get three in a row.
+
+    Block: If the opponent has two in a row, play the third to block them.
+
+    Fork: Create an opportunity where you can win in two ways.
+
+    Block Opponent's Fork:
+
+    Option 1: Create two in a row to force the opponent into defending, as long as it doesn't result in them creating a fork or winning. For example, if "X" has a corner, "O" has the center, and "X" has the opposite corner as well, "O" must not play a corner in order to win. (Playing a corner in this scenario creates a fork for "X" to win.)
+
+    Option 2: If there is a configuration where the opponent can fork, block that fork.
+
+    Center: Play the center.
+
+    Opposite Corner: If the opponent is in the corner, play the opposite corner.
+
+    Empty Corner: Play an empty corner.
+
+    Empty Side: Play an empty side.
+'''
+
+def get_cpu_move(move_list):
+    pass
+
 
 def print_board(move_list = [1,2,3,4,5,6,7,8,9]):
     line = ('\n---|---|---')
@@ -19,7 +44,7 @@ def usage():
 
 move_arr = [' ']*9
 player_move = ""
-CPU_MOVE = 'O'
+cpu_move = 'O'
 cur_move = -1
 valid_move = False
 usage()
@@ -30,14 +55,10 @@ while player_move.upper() != 'X' and player_move.upper() != 'O':
         exit()
 
 if player_move == 'O':
-    CPU_MOVE = 'X'
-    #call cpu
+    cpu_move = 'X'
+    get_cpu_move(move_arr) #call cpu, edits move_arr
+    print_board()
 
-print_board(['X','O','X','O','X','O','X','O','X'])
-#if move order is O, make initial computer move
-#else get user input
-
-#move input logicx
 while True: #while game != done
     while not valid_move:
         try:
