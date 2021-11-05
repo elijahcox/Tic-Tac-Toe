a02241df3ed03675c69b48da65a9dcb4eb417923
Elijah Cox <elijah.thomas.cox@gmail.com>
2021-11-05 00:26:48 -0500

    finish board print

--- src/tic_tac_toe.py
+++ src/tic_tac_toe.py
@@ -1,29 +1,42 @@
 #pass
 
 def print_board(move_list = [1,2,3,4,5,6,7,8,9]):
-    pass
+    line = ('\n---|---|---')
+    for i in range(1,10):
+        print(' '+str(move_list[i-1])+ ' ',end="")
+        if i % 3 != 0:
+            print('|',end="")
+        else:
+            if i != 9:
+                print(line)
+        
 
 def usage():
-    print("Welcome to impossible Tic Tac Toe!\nThe board is laid out as follows:")
+    print("Welcome to impossible Tic Tac Toe!\nThe board is laid out as follows:\n")
     print_board()
-    print("To play, simply enter 1-9 corresponding to the cell that you want to play on.")
+    print("\n\nTo play, simply enter 1-9 corresponding to the cell that you want to play on.")
 
-move_arr = [0]*9
-move_order = ""
+move_arr = [' ']*9
+player_move = ""
+CPU_MOVE = 'O'
 cur_move = -1
 valid_move = False
 usage()
 
-while move_order.upper() != 'X' and move_order.upper() != 'O':
-    move_order = str(input("Enter X to play first, O to play second. Enter Q to quit. Choice: "))
-    if move_order.upper() == 'Q':
+while player_move.upper() != 'X' and player_move.upper() != 'O':
+    player_move = str(input("Enter X to play first, O to play second. Enter Q to quit. Choice: "))
+    if player_move.upper() == 'Q':
         exit()
+
+if player_move == 'O':
+    CPU_MOVE = 'X'
+    #call cpu
 
 #if move order is O, make initial computer move
 #else get user input
 
 #move input logic
-while True:
+while True: #while game != done
     while not valid_move:
         try:
             cur_move = str(input("Enter a move[1-9] or Q to quit: "))
@@ -32,7 +45,7 @@ while True:
             else:
                 cur_move = int(cur_move)
             if cur_move in range(1,10):
-                if move_arr[cur_move-1] == 0:
+                if move_arr[cur_move-1] == ' ':
                     valid_move = True
                 else:
                    print("This spot has already been played on.")
@@ -41,3 +54,4 @@ while True:
         except ValueError:
             print("Please enter a number within [1-9].")
     valid_move = False
+    #make cpu move
