5e8de0f167dc7a0c3db25e0f3920f26f5e5fe29e
Elijah Cox <elijahtom@gmail.com>
2021-11-04 23:19:46 -0500

    finish input handling

--- src/tic_tac_toe.py
+++ src/tic_tac_toe.py
@@ -1,12 +1,11 @@
 #pass
 
-def print_board(move_list):
+def print_board(move_list = [1,2,3,4,5,6,7,8,9]):
     pass
 
 def usage():
     print("Welcome to impossible Tic Tac Toe!\nThe board is laid out as follows:")
-    moves = [1,2,3,4,5,6,7,8,9]
-    print_board(moves)
+    print_board()
     print("To play, simply enter 1-9 corresponding to the cell that you want to play on.")
 
 move_arr = [0]*9
@@ -20,12 +19,11 @@ while move_order.upper() != 'X' and move_order.upper() != 'O':
     if move_order.upper() == 'Q':
         exit()
 
+#if move order is O, make initial computer move
+#else get user input
+
 #move input logic
 while True:
-    #get move
-    #check that it is num
-    #if num, that it is [1-9]
-    #if [1-9] that it isn't taken
     while not valid_move:
         try:
             cur_move = str(input("Enter a move[1-9] or Q to quit: "))
